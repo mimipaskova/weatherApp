4c49bc1f93026809f1bfa813d8114900de9942ab
mimipaskova <maria.paskova3@gmail.com>
2020-03-14 17:06:25 +0200

    #6 create forecast component in the FE

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.scss";
+import Forecast from "./pages/forecast/forecast";
 
 function App() {
   return (
@@ -7,7 +8,9 @@ function App() {
       <header>
         <h1>Weather App</h1>
       </header>
-      <main></main>
+      <main>
+        <Forecast></Forecast>
+      </main>
       <footer>Page created by mimipaskova</footer>
     </div>
   );
