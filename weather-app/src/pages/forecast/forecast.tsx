717d898f628981de8acd2cd21882fab80d2944fc
mimipaskova <maria.paskova3@gmail.com>
2020-03-14 18:57:58 +0200

    #6 Fetch the weather data from dev server to the FE

--- weather-app/src/pages/forecast/forecast.tsx
+++ weather-app/src/pages/forecast/forecast.tsx
@@ -1,5 +1,45 @@
 import React from "react";
 
-export default function Forecast() {
-  return <div>Forecast</div>;
+type MyState = {
+  error: { message: string } | null;
+  isLoaded: boolean;
+  forecast: {};
+};
+
+export default class Forecast extends React.Component<{}, MyState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      forecast: {}
+    };
+  }
+
+  async componentDidMount() {
+    const res = await fetch("/forecast");
+    try {
+      const result = await res.json();
+      this.setState({
+        isLoaded: true,
+        forecast: JSON.stringify(result)
+      });
+    } catch (error) {
+      this.setState({
+        isLoaded: true,
+        error
+      });
+    }
+  }
+
+  render() {
+    let { error, isLoaded, forecast } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return <div>{forecast}</div>;
+    }
+  }
 }
